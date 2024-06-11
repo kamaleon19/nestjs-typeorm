d59d61b0b88312922c3dcf2c43eb2166ff00e119
kamaleon19 <joaco.px@gmail.com>
2024-06-11 20:58:56 -0300

    conexion a la base de datos

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,17 +1,34 @@
 import { Injectable, Inject } from '@nestjs/common';
 import { ConfigType } from '@nestjs/config';
+import { Client } from 'pg'; // IMPORTAMOS CLIENT DE PG PARA AGREGARLE TIPADO A LA DEPENDENCIA.
+
 import config from './config';
+import { resolve } from 'path';
 
 @Injectable()
 export class AppService {
   constructor(
     // @Inject('API_KEY') private apiKey: string,
     @Inject('TASKS') private tasks: any[],
+    @Inject('PG') private clientPG: Client, // AQUI INYECTAMOS COMO DEPENDENCIA NUESTRA CONEXION A LA BASE DE DATOS USANDO @INJECT.
     @Inject(config.KEY) private configService: ConfigType<typeof config>,
   ) {}
   getHello(): string {
     const apiKey = this.configService.apiKey;
     const name = this.configService.database.name;
     return `Hello World! ${apiKey} ${name}`;
+  }
+
+  getTasks() {
+    return new Promise((resolve, reject) => {
+      // RETORNAMOS UNA PROMESA YA QUE QUERY NOS DEVUELVE UN CALLBACK Y PARA PODER UTILIZAR ESTE METODO EN UN CONTROLADOR DEBE RETORNAR ALGO.
+      this.clientPG.query('SELECT * FROM tasks', (err, res) => {
+        // CON QUERY PODEMOS HACER CONSULTAS A LA BASE DE DATOS.
+        if (err) {
+          reject(err);
+        }
+        resolve(res.rows);
+      });
+    });
   }
 }
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,7 +1,19 @@
 import { Module, Global } from '@nestjs/common';
+import { Client } from 'pg'; // IMPORTAMOS CLIENT DE PG PARA HACER LA CONEXION DESDE NODE A LA BASE DE DATOS POSTGRES
 
 const API_KEY = '12345634';
 const API_KEY_PROD = 'PROD1212121SA';
+
+const client = new Client({
+  // AQUI DEFINIMOS LA CONEXION
+  user: 'root',
+  host: 'localhost',
+  database: 'mydb',
+  password: '123456',
+  port: 5432,
+});
+
+client.connect(); // EJECUTAMOS CONNECT PARA INICIAR LA CONEXION
 
 @Global()
 @Module({
@@ -10,7 +22,11 @@ const API_KEY_PROD = 'PROD1212121SA';
       provide: 'API_KEY',
       useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY,
     },
+    {
+      provide: 'PG',
+      useValue: client,
+    },
   ],
-  exports: ['API_KEY'],
+  exports: ['API_KEY', 'PG'],
 })
 export class DatabaseModule {}
